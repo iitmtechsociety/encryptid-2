349133ec571f8a6b7802311421a6525de29eb84d
Krishaay Jois <krishaayjois@gmail.com>
2024-01-02 11:53:11 +0530

    Add redirect for banned users

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,5 @@
 import { adminDB, adminAuth } from '$lib/server/admin'
+import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ cookies }) => {
     console.log("Loading Layout Data");
@@ -22,6 +23,8 @@ export const load = async ({ cookies }) => {
         const userDoc = await adminDB.collection('users').doc(userId).get();
         console.log("User Doc Exist: " + userDoc.exists);
         if (userDoc.exists) {
+            const userData = userDoc.data();
+            if(userData.banned) return redirect(306,'/banned');
             return {
                 userId: userId,
                 registration_state: "completed"
---
+++ src/routes/banned/+page.svelte
@@ -0,0 +1,4 @@
+<center>
+    <span class="font-bold text-3xl">You Have Been</span><br/>
+    <span class="text-error text-9xl" style="font-family: 'Rubik Glitch' !important;">BANNED</span><br/>
+</center>
