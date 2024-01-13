506a4b95bae8607049407beb08eeb5be5d922858
Krishaay Jois <krishaayjois@gmail.com>
2024-01-13 12:27:11 +0530

    Removed encryptid firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,6 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 export const app = initializeApp({
-    apiKey: "AIzaSyCi8mCw5ICLJZYN-VMftWQTMYCJ669rqf0",
-    authDomain: "encryptid-2-c0c54.firebaseapp.com",
-    projectId: "encryptid-2-c0c54",
-    storageBucket: "encryptid-2-c0c54.appspot.com",
-    messagingSenderId: "399101831807",
-    appId: "1:399101831807:web:39df7986ab54b7b3382d4a",
-    measurementId: "G-9DBDWJZZ1T"
   });
 
 export const firestore = getFirestore(app);
